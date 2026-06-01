dbc25398c43bd1a8ec0f51fb4f32636bcf2f10c0
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-06-01 21:30:00 +0100

    added skeleton layout to tree class

--- classes/tree.js
+++ classes/tree.js
@@ -1,5 +1,53 @@
 export class Tree {
     constructor() {
+        this.root = null;
+    }
 
+    buildTree(array) {
+
+    }
+
+    includes(value) {
+
+    }
+
+    insert(value) {
+
+    }
+
+    deleteItem(value) {
+
+    }
+
+    levelOrderForEach(callback) {
+
+    }
+
+    inOrderForEach(callback) {
+
+    }
+
+    preOrderForEach(callback) {
+
+    }
+
+    postOrderForEach(callback) {
+
+    }
+
+    height(value) {
+
+    }
+
+    depth(value) {
+
+    }
+
+    isBalanced() {
+
+    }
+
+    rebalance() {
+        
     }
 }
