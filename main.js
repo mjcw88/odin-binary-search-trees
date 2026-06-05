a4c50da9e0f596f76be62410204821ac85daaeca
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-06-05 15:45:56 +0100

    filled out main.js to spec

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
 import { Tree } from "./classes/tree.js";
 
-const tree = new Tree();
+const createRandomArray = () => {
+    const SIZE = 20;
+    const MAX = 101;
+    const array = [];
+
+    for(let i = 0; i < SIZE; i++) {
+        array.push(Math.floor(Math.random() * MAX))
+    }
+    return array;
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null || node === undefined) return;
+
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+const array = createRandomArray();
+const tree = new Tree(array);
+const REPEAT = 150;
+
+console.log("-".repeat(REPEAT));
+prettyPrint(tree.root);
+console.log("Is balanced: " + tree.isBalanced());
+
+const values = [150, 101, 666, 6345, 12940];
+values.forEach(v => {
+    tree.insert(v);
+})
+
+console.log("-".repeat(REPEAT));
+prettyPrint(tree.root);
+console.log("Is balanced: " + tree.isBalanced());
+
+tree.rebalance();
+
+console.log("-".repeat(REPEAT));
+prettyPrint(tree.root);
+console.log("Is balanced: " + tree.isBalanced());
+
+let string = "";
+console.log("-".repeat(REPEAT));
+tree.levelOrderForEach(value => string = string + value + " ");
+console.log("Level Order: " + string);
+
+string = "";
+console.log("-".repeat(REPEAT));
+tree.preOrderForEach(value => string = string + value + " ");
+console.log("Pre-order: " + string);
+
+string = "";
+console.log("-".repeat(REPEAT));
+tree.postOrderForEach(value => string = string + value + " ");
+console.log("Post-order: " + string);
+
+string = "";
+console.log("-".repeat(REPEAT));
+tree.inOrderForEach(value => string = string + value + " ");
+console.log("In-order: " + string);
